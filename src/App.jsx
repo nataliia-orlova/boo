0a81137d0ec78c2479651465894a05716bdf4692
Nataliia Orlova <nataliia.orlova.dev@gmail.com>
2023-12-06 17:59:27 +0100

    Add components and db

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './scss/styles.css';
+import { useState, useEffect } from 'react';
 
 import Header from './components/Header';
 import ProductsList from './components/ProductsList';
@@ -6,10 +7,43 @@ import Form from './components/Form';
 import Footer from './components/Footer';
 
 function App() {
+    const [initialProducts, setInitialProducts] = useState([]);
+    let products = initialProducts;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/products');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setInitialProducts(data);
+            } catch (error) {
+                console.error('There was a problem fetching the data: ', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const [sortedProducts, setSortedProducts] = useState(products);
+
+    const handleSelect = (index) => {
+        let currentValueIndex = index;
+        if (currentValueIndex == 1) {
+            setSortedProducts(products.sort((a, b) => b.rating - a.rating));
+        } else if (currentValueIndex == 2) {
+            setSortedProducts(products.sort((a, b) => b.price - a.price));
+        } else if (currentValueIndex == 3) {
+            setSortedProducts(products.sort((a, b) => a.price - b.price));
+        }
+    };
+
     return (
         <>
             <Header />
-            <ProductsList />
+            <ProductsList products={products} handleSelect={handleSelect} />
             <Form />
             <Footer />
         </>
---
+++ src/components/Sort.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Sort({ products, handleSelect }) {
+    return (
+        <div className='product-list__sort'>
+            <label htmlFor='select'>Sort by</label>
+
+            <select
+                value={products}
+                onChange={(e) => handleSelect(e.target.selectedIndex)}
+                name='sort'
+                id='select'
+            >
+                <option value=''>Choose</option>
+                <option value='rating'>Popularity</option>
+                <option value='ascending'>Price ascending</option>
+                <option value='descending'>Price descending</option>
+            </select>
+        </div>
+    );
+}
